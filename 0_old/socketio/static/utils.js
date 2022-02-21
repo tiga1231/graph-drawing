9878bc2fc61cc6f39ded735e5a2f5a6b6383ce97
tiga1231 <2913895+tiga1231@users.noreply.github.com>
2022-02-21 12:28:43 -0600

    rmoved 0_old/

--- 0_old/socketio/static/utils.js
+++
@@ -1,7 +0,0 @@
-let utils = {};
-
-utils.graph2data = (graph)=>{
-  let nodes = graph.nodes.map(i=>({id:i}));
-  let edges = graph.edges.map((e)=>({source:e[0], target:e[1]}));
-  return {nodes, edges};
-};
